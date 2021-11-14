62267bf50316bf29923aed90431dcf556774bd6d
ecj222 <enochchejieh@gmail.com>
2021-11-14 22:03:55 +0100

    chore: add latest articles

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -15,22 +15,10 @@ const About = () => {
             }}
           />
           <p>
-            I&apos;m a software developer and technical writer based in Nigeria
+            I&apos;m a software developer and content creator based in Nigeria
             with a love for open source and exploring new things.
           </p>
           <em>„Faber est Suae Quisque Fortunae“</em>
-          <ul>
-            <li>Html</li>
-            <li>Css</li>
-            <li>Javascript</li>
-            <li>Typescript</li>
-            <li>React.Js</li>
-            <li>Redux</li>
-            <li>Vue.Js</li>
-            <li>Vuex</li>
-            <li>Nuxt.Js</li>
-            <li>Node / Express.Js</li>
-          </ul>
         </div>
         <Profile />
       </div>
